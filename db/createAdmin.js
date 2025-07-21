3ebf05b2fc7bb8dcbad9bcde24be55f98a892995
samuel <samawyck@gmail.com>
2025-07-21 16:25:03 -0500

    Setup basic structure and define db schema

---
+++ db/createAdmin.js
@@ -0,0 +1,25 @@
+const {PrismaClient} = require("../generated/prisma");
+const bcrypt = require("bcryptjs");
+
+
+const prisma = new PrismaClient();
+const pwdHash = bcrypt.hashSync(process.argv[5]);
+
+
+async function main() {
+    console.log("creating admin...");
+    await prisma.user.create({
+        data: {
+            firstname: process.argv[2],
+            lastname: process.argv[3],
+            username: process.argv[3],
+            password: pwdHash,
+            isAdmin: true
+        }
+    });
+    console.log("done");
+};
+
+main().then(function() {
+    prisma.$disconnect();
+});
---
+++ prisma/migrations/20250721201432_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "title" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "posted" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "editedAt" TIMESTAMP(3),
+    "authorId" TEXT NOT NULL,
+    "parentId" TEXT NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
