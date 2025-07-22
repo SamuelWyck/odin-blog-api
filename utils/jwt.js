b898d3e9196d8e00d3fec5d1b784f3fd2337d4bf
samuel <samawyck@gmail.com>
2025-07-22 15:42:21 -0500

    Create util functions for jwt handling

---
+++ utils/jwt.js
@@ -0,0 +1,50 @@
+const jwt = require("jsonwebtoken");
+
+
+
+function generateToken(user) {
+    const token = jwt.sign(
+        user, 
+        process.env.TOKEN_SECRET,
+    );
+    return token;
+};
+
+
+function verifyToken(token) {
+    const user = jwt.verify(
+        token,
+        process.env.TOKEN_SECRET,
+        function(error, user) {
+            if (error) {
+                return null;
+            }
+            return user;
+        }
+    );
+
+    return user;
+};
+
+
+function parseHeaderToken(req) {
+    const bearerHeader = req.headers["authorization"];
+    if (!bearerHeader) {
+        return null;
+    }
+
+    const tokenIndex = 1;
+    const token = bearerHeader.split(" ")[tokenIndex];
+    if (!token) {
+        return null;
+    }
+
+    return token;
+};
+
+
+module.exports = {
+    generateToken,
+    verifyToken,
+    parseHeaderToken
+};
