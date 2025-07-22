408146d91c3d52e7a9fa5754fb47b7f3a2cc9a82
samuel <samawyck@gmail.com>
2025-07-22 15:43:05 -0500

    Config passport

---
+++ utils/passport.js
@@ -0,0 +1,38 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const db = require("../db/querys.js");
+const bcrypt = require("bcryptjs");
+
+
+passport.use(
+    new LocalStrategy(async function(username, password, done) {
+        const errorMessage = "Incorrect username or password";
+
+        try {
+            const user = await db.findUniqueUser({
+                where: {
+                    username: username
+                }
+            });
+
+            if (!user) {
+                return done(null, false, {msg: errorMessage});
+            }
+
+            const match = bcrypt.compare(password, user.password);
+
+            if (!match) {
+                return done(null, false, {msg: errorMessage});
+            }
+
+            delete user.password;
+            return done(null, user);
+
+        } catch (error) {
+            return done(error);
+        }
+    })
+);
+
+
+module.exports = passport;
