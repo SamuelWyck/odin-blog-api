32e768bc13a25e762ac671070310f24af79d4d1f
samuel <samawyck@gmail.com>
2025-07-22 15:46:03 -0500

    Create auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,77 @@
+const asynchandler = require("express-async-handler");
+const {validationResult} = require("express-validator");
+const {signupVal} = require("../utils/validator.js");
+const bcrypt = require("bcryptjs");
+const db = require("../db/querys.js");
+const passport = require("../utils/passport.js");
+const jwt = require("../utils/jwt.js");
+
+
+
+const signUpPost = asynchandler(async function(req, res, next) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.json({
+            errors: errors.array()
+        });
+    }
+
+    const username = req.body.username.trim();
+    const firstName = req.body.firstname.trim();
+    const lastName = req.body.lastname.trim();
+    const pwdHash = bcrypt.hash(req.body.password);
+
+
+    try {
+        await db.createUser({
+            data: {
+                username: username,
+                firstName: firstName,
+                lastname: lastName,
+                password: pwdHash,
+                isAdmin: false
+            }
+        });
+    } catch (error) {
+        return res.json({errors: [{msg: "Internal Error"}]})
+    }
+
+    next();
+});
+
+
+const loginPost = asynchandler(async function(req, res) {
+    const authFunction = passport.authenticate(
+        "local", 
+        function(error, user, msg) {
+            if (error) {
+                return res.status(500).json({errors: [{msg: "Internal Error"}]});
+            }
+            if (msg) {
+                return res.status(401).json({errors: [msg]});
+            }
+
+            req.login(user, {session: false}, function(error) {
+                if (error) {
+                    return res.send(error);
+                }
+                const token = jwt.generateToken(user);
+                return res.json({user, token});
+            });
+        }
+    );
+
+    authFunction(req, res);
+});
+
+
+
+module.exports = {
+    signUpPost: [
+        signupVal,
+        signUpPost,
+        loginPost
+    ],
+    loginPost
+};
