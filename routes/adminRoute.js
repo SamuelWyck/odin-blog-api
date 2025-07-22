4c20391ce25de64f249d179701b24b66624d90ba
samuel <samawyck@gmail.com>
2025-07-22 17:54:43 -0500

    Create admin route

---
+++ routes/adminRoute.js
@@ -0,0 +1,22 @@
+const {Router} = require("express");
+const adminController = require("../controllers/adminController.js");
+const {isAdmin, authUser} = require("../utils/authMiddleware.js");
+
+
+
+const adminRoute = Router();
+
+
+adminRoute.use(authUser);
+adminRoute.use(isAdmin);
+
+adminRoute.get("/posts", adminController.adminPostsGet);
+adminRoute.get("/posts/:postId", adminController.adminPostsGet);
+adminRoute.post("/posts/new", adminController.adminNewPostPost);
+adminRoute.put("/posts/edit/:postId", adminController.adminEditPostPut);
+adminRoute.delete("/posts/delete/:postId", adminController.adminDeletePostDelete);
+adminRoute.delete("/comments/delete/:commentId", adminController.adminDeleteCommentDelete);
+
+
+
+module.exports = adminRoute;
