d526cc1a83e7c5b289d615cfc0ac449f8bf68094
samuel <samawyck@gmail.com>
2025-07-23 09:08:25 -0500

    Create handlers to create, update, and delete comments

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,26 +1,84 @@
 const asynchandler = require("express-async-handler");
 const db = require("../db/querys.js");
+const {commentVal} = require("../utils/validator.js");
+const {validationResult} = require("express-validator");
+const datetime = require("datetime");
 
 
 
 const newCommentPost = asynchandler(async function(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
 
+    const text = req.body.text.trim();
+    const userId = req.user.id;
+    const postId = req.body.postid;
+
+    await db.createComment({
+        data: {
+            text: text,
+            authorId: userId,
+            parentId: postId
+        }
+    });
+
+    return res.status(201).json({data: "Success"});
 });
 
 
 const editCommentPut = asynchandler(async function(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
 
+    const text = req.body.text.trim();
+    const userId = req.user.id;
+    const postId = req.body.postid;
+    const commentId = req.params.commentId;
+
+    await db.editComment({
+        where: {
+            id: commentId,
+            authorId: userId,
+            parentId: postId
+        },
+        data: {
+            text: text,
+            editedAt: now()
+        }
+    });
+
+    return res.json({data: "Success"});
 });
 
 
 const deleteCommentDelete = asynchandler(async function(req, res) {
+    const commentId = req.params.commentId;
+    const userId = req.user.id;
 
+    await db.deleteComment({
+        where: {
+            id: commentId,
+            authorId: userId
+        }
+    });
+
+    return res.json({data: "Success"});
 });
 
 
 
 module.exports = {
-    newCommentPost,
-    editCommentPut,
+    newCommentPost: [
+        commentVal,
+        newCommentPost
+    ],
+    editCommentPut: [
+        commentVal,
+        editCommentPut
+    ],
     deleteCommentDelete
 };
