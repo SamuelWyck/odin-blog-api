84b67ca1c849f6811d707d01c5a0fce1ec8712f6
samuel <samawyck@gmail.com>
2025-07-23 09:52:11 -0500

    Finish admin handlers and remove sensitive info from responses

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,7 @@
 const asynchandler = require("express-async-handler");
 const db = require("../db/querys.js");
+const {validationResult} = require("express-validator");
+const {postVal} = require("../utils/validator.js");
 
 
 
@@ -14,7 +16,11 @@ const adminPostsGet = asynchandler(async function(req, res) {
             createdAt: "desc"
         },
         include: {
-            author: true
+            author: {
+                select: {
+                    username: true
+                }
+            }
         }
     });
 
@@ -40,7 +46,11 @@ const adminPostGet = asynchandler(async function(req, res) {
                     createdAt: "desc"
                 },
                 include: {
-                    author: true
+                    author: {
+                        select: {
+                            username: true
+                        }
+                    }
                 }
             }
         }
@@ -50,23 +60,79 @@ const adminPostGet = asynchandler(async function(req, res) {
 });
 
 
+const adminNewPostPost = asynchandler(async function(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const authorId = req.user.id;
+    const title = req.body.title.trim();
+    const text = req.body.text.trim();
+
+    await db.createPost({
+        data: {
+            authorId: authorId,
+            title: title,
+            text: text
+        }
+    });
+
+    return res.status(201).json({data: "Success"});
+});
+
+
 const adminEditPostPut = asynchandler(async function(req, res) {
-    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const postId = req.params.postId;
+    const authorId = req.user.id;
+    const text = req.body.text.trim();
+    const title = req.body.title.trim();
+
+    await db.editPost({
+        where: {
+            id: postId,
+            authorId: authorId
+        },
+        data: {
+            text: text,
+            title: title
+        }
+    });
+
+    return res.json({data: "Success"});
 });
 
 
 const adminDeletePostDelete = asynchandler(async function(req, res) {
+    const postId = req.params.postId;
+    const authorId = req.user.id;
 
-});
+    await db.deletePost({
+        where: {
+            id: postId,
+            authorId: authorId
+        }
+    });
 
-
-const adminNewPostPost = asynchandler(async function(req, res) {
-
+    return res.json({data: "Success"});
 });
 
 
 const adminDeleteCommentDelete = asynchandler(async function(req, res) {
+    const commentId = req.params.commentId;
 
+    await db.deleteComment({
+        where: {
+            id: commentId
+        }
+    });
+
+    return res.json({data: "Success"});
 });
 
 
@@ -74,8 +140,14 @@ const adminDeleteCommentDelete = asynchandler(async function(req, res) {
 module.exports = {
     adminPostsGet,
     adminPostGet,
-    adminEditPostPut,
-    adminNewPostPost,
+    adminNewPostPost: [
+        postVal,
+        adminNewPostPost
+    ],
+    adminEditPostPut: [
+        postVal,
+        adminEditPostPut
+    ],
     adminDeletePostDelete,
     adminDeleteCommentDelete
 };
