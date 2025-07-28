ec8bbb81cb336c2aa477fa6cb7f9185fa2a923c5
samuel <samawyck@gmail.com>
2025-07-28 16:50:44 -0500

    Add preview field to post model

---
+++ prisma/migrations/20250728213138_add_preview_to_post/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "preview" TEXT NOT NULL DEFAULT 'this is a long preview string to see how things look it is supposed to be a teaser for the post cool';
