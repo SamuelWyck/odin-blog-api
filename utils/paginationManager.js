30c9121f77a8c4c1fc506dc73e6c84b3205107cb
samuel <samawyck@gmail.com>
2025-07-29 16:59:01 -0500

    Add logic for comment pagination

--- utils/paginationManager.js
+++ utils/paginationManager.js
@@ -1,11 +1,16 @@
 class PaginationManager {
     constructor() {
         this.postTakeNumber = 10;
+        this.commentTakeNumber = 20;
     };
 
 
     calcPostSkipNumber(pageNum) {
         return pageNum * this.postTakeNumber;
+    };
+
+    clacCommentSkipNumber(pageNum) {
+        return pageNum * this.commentTakeNumber;
     };
 };
 
