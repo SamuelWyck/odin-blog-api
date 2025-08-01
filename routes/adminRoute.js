bf1cf10b40d1bbe821e737b85830af2eb69111c2
samuel <samawyck@gmail.com>
2025-08-01 14:15:52 -0500

    Install multer middleware on post CRUD endpoints

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,6 +1,7 @@
 const {Router} = require("express");
 const adminController = require("../controllers/adminController.js");
 const {isAdmin, authUser} = require("../utils/authMiddleware.js");
+const upload = require("../utils/multer.js");
 
 
 
@@ -12,8 +13,8 @@ adminRoute.use(isAdmin);
 
 adminRoute.get("/posts", adminController.adminPostsGet);
 adminRoute.get("/posts/:postId", adminController.adminPostGet);
-adminRoute.post("/posts/new", adminController.adminNewPostPost);
-adminRoute.put("/posts/edit/:postId", adminController.adminEditPostPut);
+adminRoute.post("/posts/new", upload.single("image"), adminController.adminNewPostPost);
+adminRoute.put("/posts/edit/:postId", upload.single("image"), adminController.adminEditPostPut);
 adminRoute.delete("/posts/delete/:postId", adminController.adminDeletePostDelete);
 adminRoute.delete("/comments/delete/:commentId", adminController.adminDeleteCommentDelete);
 adminRoute.get("/apikey", adminController.adminGetApiKeyGet);
